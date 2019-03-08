aa5d7f7aa314a2586b261c4cc9041a173fd5f6dd
Lunarbaby <lunarbaby21@hotmail.com>
2019-03-08 13:28:57 -0500

    Added the rest of the files for week eight

--- Ajax exercise/index.php
+++ Ajax exercise/index.php
@@ -4,8 +4,24 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: "origin, x-requested-with, content-type"');
 header('Access-Control-Allow-Methods "GET, POST, OPTIONS"');
-$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
-$computedString = "Hi, " . $name . "!";
+if(isset($_POST['userName']) && !empty($_POST['userName'])) {
+  $name = $_POST['userName'];
+  $computedString = "Post Success: Hi, " . $name . "!";
+} else if(isset($_GET['userName']) && !empty($_GET['userName'])) {
+  $name = $_GET['userName'];
+  $computedString = "GET Success: Hi, " . $name . "!";
+} else if ($_POST) {
+  $name = $_POST;
+  $computedString = "Seems like there was something wrong with your POST " . $name . "!";
+} else if ($_GET) {
+  $name = $_GET;
+  $computedString = "Seems like there was something wrong with your GET " . $name . "!";
+} else {
+  $name = "no name";
+  $computedString = "Seems like my error handling is broke " . $name . "!";
+}
+// $name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
+// $computedString = "Hi, " . $name . "!";
 $array = ['userName' => $name, 'computedString' => $computedString];
 echo json_encode($array);
 ?>
